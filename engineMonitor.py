889d440f6f3489e31d7c5254ea3cfd65db096dd7
liang.tang <liang.tang.eddie@gmail.com>
2020-12-03 21:46:56 -0800

    add data field

--- engineMonitor.py
+++ engineMonitor.py
@@ -21,12 +21,53 @@ port_list = list(serial.tools.list_ports.comports())
 # for port in port_list:
 #     sg.Print(port, do_not_reroute_stdout=False)
 
+# 发动机转速 Engine Speed                   r/min
+# 发动机油耗 Fuel Flow Rate                 L/h
+# 排气口气压 Manifold Air Pressure          hPa
+# 润滑油油压 Oil Pressure,Absolute          hPa
+# 润滑油温度 Oil Temperature                K
+# 冷却液温度 Coolant Temperature            K
+
+# 气缸1温度  EGT Cylinder 1                 K
+# 气缸2温度  EGT Cylinder 2                 K
+# 气缸3温度  EGT Cylinder 3                 K
+# 气缸4温度  EGT Cylinder 4                 K
+# 排气口气温 Manifold Air Temperature       K
+# 发动机环温 Engine Ambient Temperature     K
+
+# 油门位置   Throttle Position              %
+# 发动机环压 Engine Ambient Pressure        hPa
+
+# ECU总线电压 ECU Bus Voltage               V
+# 润滑油补偿油压 Oil Pressure Compensated   hPa
+
 sg.theme('BluePurple')
 
-layout = [[sg.Text('Your typed chars appear here:'), sg.Text(size=(15,1), key='-OUTPUT-')],
-          [sg.Text('发动机功率:'), sg.Text(size=(15,1), key='-POWER-'),
-          sg.Text('发动机转速:'), sg.Text(size=(15,1), key='-RPM-'),
-          sg.Text('发动机温度:'), sg.Text(size=(15,1), key='-TEMP-')],
+layout = [#[sg.Text('Your typed chars appear here:'), sg.Text(size=(15,1), key='-OUTPUT-')],
+          [
+            sg.Text('发动机转速:', size=(10,1)), sg.Text(size=(10,1), key='-SPEED-'),
+            sg.Text('发动机油耗:', size=(10,1)), sg.Text(size=(10,1), key='-FLOW-'),
+            sg.Text('发动机环温:', size=(10,1)), sg.Text(size=(10,1), key='-EAT-'),
+            sg.Text('发动机环压:', size=(10,1)), sg.Text(size=(10,1), key='-EAP-')
+          ],
+          [
+            sg.Text('润滑油油压:', size=(10,1)), sg.Text(size=(10,1), key='-OP-'),
+            sg.Text('润滑油温度:', size=(10,1)), sg.Text(size=(10,1), key='-OT-'),
+            sg.Text('补偿油压:', size=(10,1)), sg.Text(size=(10,1), key='-OPC-'),
+            sg.Text('冷却液温度:', size=(10,1)), sg.Text(size=(10,1), key='-CT-')
+          ],
+          [
+            sg.Text('气缸1温度:', size=(10,1)), sg.Text(size=(10,1), key='-EGTC1-'),
+            sg.Text('气缸2温度:', size=(10,1)), sg.Text(size=(10,1), key='-EGTC2-'),
+            sg.Text('气缸3温度:', size=(10,1)), sg.Text(size=(10,1), key='-EGTC3-'),
+            sg.Text('气缸4温度:', size=(10,1)), sg.Text(size=(10,1), key='-EGTC4-')
+          ],
+          [
+            sg.Text('油门位置:', size=(10,1)), sg.Text(size=(10,1), key='-THR-'),
+            sg.Text('排气口气温:', size=(10,1)), sg.Text(size=(10,1), key='-MAT-'),
+            sg.Text('排气口气压:', size=(10,1)), sg.Text(size=(10,1), key='-MAP-'),
+            sg.Text('ECU电压:', size=(10,1)), sg.Text(size=(10,1), key='-EBV-')
+          ],
           [sg.Input(key='-IN-')],
           [sg.Combo(port_list)],
           [sg.Combo(['11', '22', '33'])],
@@ -45,12 +86,42 @@ while True:  # Event Loop
         # Update the "output" text element to be the value of "input" element
         window['-OUTPUT-'].update(values['-IN-'])
 
-    str_line = str(val) + " ℃"
-    window['-TEMP-'].update(str_line)
-    str_line = str(val) + " W"
-    window['-POWER-'].update(str_line)
-    str_line = str(val) + " RPM"
-    window['-RPM-'].update(str_line)
+    str_line = str(val) + " r/min"
+    window['-SPEED-'].update(str_line)
+    str_line = str(val) + " L/h"
+    window['-FLOW-'].update(str_line)
+    str_line = str(val) + " K"
+    window['-EAT-'].update(str_line)
+    str_line = str(val) + " hPa"
+    window['-EAP-'].update(str_line)
+
+    str_line = str(val) + " hPa"
+    window['-OP-'].update(str_line)
+    str_line = str(val) + " K"
+    window['-OT-'].update(str_line)
+    str_line = str(val) + " hPa"
+    window['-OPC-'].update(str_line)
+    str_line = str(val) + " K"
+    window['-CT-'].update(str_line)
+
+    str_line = str(val) + " K"
+    window['-EGTC1-'].update(str_line)
+    str_line = str(val) + " K"
+    window['-EGTC2-'].update(str_line)
+    str_line = str(val) + " K"
+    window['-EGTC3-'].update(str_line)
+    str_line = str(val) + " K"
+    window['-EGTC4-'].update(str_line)
+
+    str_line = str(val) + " %"
+    window['-THR-'].update(str_line)
+    str_line = str(val) + " K"
+    window['-MAT-'].update(str_line)
+    str_line = str(val) + " hPa"
+    window['-MAP-'].update(str_line)
+    str_line = str(val) + " V"
+    window['-EBV-'].update(str_line)
+
     val += 1
     time.sleep(0.01)
 
